827a015c525fe2f07c5cbbbf253cd6f17142c7a1
zgt <2506526350@qq.com>
2016-09-20 11:05:38 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogActiveRecord::Application.config.secret_token = '74fe7ebaa3e882d341906d9fb70efee20ec868847cf53e32f9ea15da17411fb3a0617d7e62b66a2039a2dca192bd0e4823ecd78d7a362161a4903fbb58d7af6e'
